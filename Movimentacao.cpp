22da6ee47c6dd4a105c0135d726c70eda2185275
Hugo Ferreira <hugofm2@ufmg.br>
2019-09-26 11:49:14 -0300

    Classe Movimentacao Adicionada

---
+++ Movimentacao.cpp
@@ -0,0 +1,33 @@
+#include "Movimentacao.h"
+
+Movimentacao::Movimentacao(Date date, std::string d, char dC, double v){
+  this-> dataMov = date;
+  this-> descricao = d;
+  this-> debitoCredito = dC;
+  this-> valor = v;
+}
+void Movimentacao::setdataMov(int d, int m,int y){
+  dataMov.SetDate(d,m,y);
+}
+void Movimentacao::setDescricao(std::string desc){
+  descricao = desc;
+}
+void Movimentacao::setDebitoCredito(char debCred){
+  debitoCredito = debCred;
+}
+void Movimentacao::setValor(double value){
+  valor = value;
+}
+
+std::string Movimentacao::getDescricao(){
+  return descricao;
+}
+char Movimentacao::getDebitoCredito(){
+  return debitoCredito;
+}
+double Movimentacao::getValor(){
+  return valor;
+}
+Date Movimentacao::getDate(){
+  return dataMov;
+}
---
+++ Movimentacao.h
@@ -0,0 +1,24 @@
+#ifndef MOVIMENTACAO_H
+#define MOVIMENTACAO_H
+#include <iostream>
+#include "Date.h"
+
+class Movimentacao
+{
+private:
+  Date dataMov;
+  std::string descricao;
+  char debitoCredito;
+  double valor;
+public:
+  Movimentacao(Date date, std::string d, char dC, double v);
+  void setdataMov(int d,int m,int y);
+  void setDescricao(std::string desc);
+  void setDebitoCredito(char debCred);
+  void setValor(double value);
+  std::string getDescricao();
+  char getDebitoCredito();
+  double getValor();
+  Date getDate();
+};
+#endif
