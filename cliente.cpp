180a06a98864fa642b15d9b4538ceb6b59979408
Hugo Ferreira <hugofm2@ufmg.br>
2019-09-26 11:47:46 -0300

    Classe Cliente Adicionada

---
+++ cliente.cpp
@@ -0,0 +1,41 @@
+#include "Cliente.h"
+
+Cliente::Cliente(){
+  nomeCliente = "";
+  cpf_cnpj = "";
+  endereco = "";
+  fone = "";
+}
+Cliente::Cliente(std::string n, std::string c, std::string e, std::string f){
+  // std::cout << "Construtor chamado" << std::endl;
+  this ->nomeCliente = n;
+  this ->cpf_cnpj = c;
+  this ->endereco = e;
+  this ->fone = f;
+}
+
+std::string Cliente::getNome(){
+  return nomeCliente;
+}
+std::string Cliente::getCpf_cnpj(){
+  return cpf_cnpj;
+}
+std::string Cliente::getEndereco(){
+  return endereco;
+}
+std::string Cliente::getFone(){
+  return fone;
+}
+
+void Cliente::setNome(std::string input){
+  nomeCliente = input;
+}
+void Cliente::setCpf_cnpj(std::string input){
+  cpf_cnpj = input;
+}
+void Cliente::setEndereco(std::string input){
+  endereco = input;
+}
+void Cliente::setFone(std::string input){
+  fone = input;
+}
---
+++ cliente.h
@@ -0,0 +1,33 @@
+// #include <bits/stdc++.h>
+// #include <ctime>
+// #include <vector>
+// using namespace std;
+
+
+#ifndef CLIENTE_H
+#define CLIENTE_H
+#include <bits/stdc++.h>
+
+
+class Cliente {
+private:
+  std::string nomeCliente;
+  std::string cpf_cnpj;
+  std::string endereco;
+  std::string fone;
+
+    // Access specifier
+    public:
+      Cliente();
+      Cliente(std::string n, std::string c, std::string e, std::string f);
+      std::string getNome();
+      std::string getCpf_cnpj();
+      std::string getEndereco();
+      std::string getFone();
+      void setNome(std::string input);
+      void setCpf_cnpj(std::string input);
+      void setEndereco(std::string input);
+      void setFone(std::string input);
+};
+
+#endif
